12f7d796e616518b8646432a95ff486c5ec25d57
EnigmaCurry <ryan@enigmacurry.com>
2025-12-29 15:15:31 -0700

    delete vestigal

--- frontend/src/routes/login/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import LoginForm from "$lib/components/login_oidc.svelte";
-</script>
-<div class="flex h-screen w-full items-center justify-center px-4">
-  <LoginForm id="login" action="/api/login" />
-</div>
